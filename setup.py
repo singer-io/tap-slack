f60d58199a2331c7363eb89f8d69a4401f3e8087
Jacob Baca <jbaca@stitchdata.com>
2020-07-14 13:50:07 +0000

    made major version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-slack',
-      version='0.1.0',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Slack Web API',
       author='dwallace@envoy.com',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
