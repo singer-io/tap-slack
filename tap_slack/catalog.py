056041582321ed9a3392eae822c0d4013a0222ff
Tushar Mittal <tushar.mittal@qlik.com>
2025-09-18 16:23:36 +0530

    parent-tap-stream-id added

--- tap_slack/catalog.py
+++ tap_slack/catalog.py
@@ -6,15 +6,25 @@ def generate_catalog(streams):
     catalog['streams'] = []
     for stream in streams:
         schema = stream.load_schema()
+
+        mdata = singer.metadata.get_standard_metadata(
+            schema=schema,
+            key_properties=stream.key_properties,
+            valid_replication_keys=stream.valid_replication_keys,
+            replication_method=stream.replication_method
+        )
+        mdata = singer.metadata.to_map(mdata)
+
+        parent_tap_stream_id = getattr(stream, "parent", None)
+        if parent_tap_stream_id:
+            mdata = singer.metadata.write(mdata, (), 'parent-tap-stream-id', parent_tap_stream_id)
+
+        mdata = singer.metadata.to_list(mdata)        
         catalog_entry = {
             'stream': stream.name,
             'tap_stream_id': stream.name,
             'schema': schema,
-            'metadata': singer.metadata.get_standard_metadata(
-                schema=schema,
-                key_properties=stream.key_properties,
-                valid_replication_keys=stream.valid_replication_keys,
-                replication_method=stream.replication_method)
+            'metadata': mdata
         }
         catalog['streams'].append(catalog_entry)
 
